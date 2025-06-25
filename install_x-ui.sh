883c981f4f6295f41ddae951244c1658189730d3
GrumpyMonka <40570589+GrumpyMonka@users.noreply.github.com>
2025-06-25 14:04:09 +0300

    add script

---
+++ install_x-ui.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Скрипт установки Xray и 3X-UI на Ubuntu 20.04+
+
+# Обновление системы
+apt update
+
+# Установка необходимых пакетов
+apt install -y curl wget unzip
+
+# Установка Xray
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
+
+# Установка 3X-UI
+bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+
+# Настройка фаервола
+ufw allow 80
+ufw allow 443
+ufw enable
+
+# Перезапуск службы 3X-UI
+systemctl restart x-ui
